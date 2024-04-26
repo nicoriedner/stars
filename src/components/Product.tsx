369c14f0dac03aa77d81a665542a45bd78c1a9d5
nico <146178362+nicoriedner@users.noreply.github.com>
2024-04-26 09:08:26 +0200

    add Products

---
+++ src/components/Product.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Product.propTypes = {
+
+};
+
+function Product() {
+    return (
+        <div></div>
+    );
+}
+
+export default Product;
